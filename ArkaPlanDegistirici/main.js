c738ef6522a02e0076b380371154b4ad6e9a4829
kubranuryavuz <nurkacm@gmail.com>
2025-05-16 14:37:59 +0300

    first commit

---
+++ ArkaPlanDegistirici/main.js
@@ -0,0 +1,27 @@
+let buttonRed = document.getElementById("btn-red");
+let buttonBlue = document.getElementById("btn-blue");
+let buttonBlack = document.getElementById("btn-black");
+
+buttonRed.addEventListener("click", function () {
+  document.body.style.background = "red";
+  localStorage.setItem("bg", "red");
+});
+
+buttonBlue.addEventListener("click", function () {
+  document.body.style.background = "blue";
+  localStorage.setItem("bg", "blue");
+});
+
+buttonBlack.addEventListener("click", function () {
+  document.body.style.background = "black";
+  localStorage.setItem("bg", "black");
+});
+
+function storageBackground() {
+  if (localStorage.getItem("bg") != "") {
+    document.body.style.background = localStorage.getItem("bg");
+  } else {
+    return true;
+  }
+}
+storageBackground();
