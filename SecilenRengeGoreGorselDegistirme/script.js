6e7afef20673328b71071e5f248640c504e38156
kubranuryavuz <nurkacm@gmail.com>
2025-05-19 22:14:39 +0300

    Add hoodie color switch functionality

---
+++ SecilenRengeGoreGorselDegistirme/script.js
@@ -0,0 +1,9 @@
+let imageShow = document.getElementById("imageChange");
+
+function greenChange() {
+  imageShow.src = "./assets/image/yesilhoodie.png";
+}
+
+function purpleChange() {
+  imageShow.src = "./assets/image/morhoodie.png";
+}
